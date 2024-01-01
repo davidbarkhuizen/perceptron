b723d01e1208400128efbc11696c50c7c071f882
david barkhuizen <david.barkhuizen@gmail.com>
2024-01-01 12:21:45 +0200

    graph perceptron

--- python3/perceptron/perceptron/linear_classifier.py
+++ python3/perceptron/perceptron/linear_classifier.py
@@ -1,116 +1,157 @@
 from __future__ import annotations
 
 from random import uniform
+from statistics import mean
+from matplotlib import pyplot, lines
+
+import logging
 
 class SPoint:
 
-    def __init__(self, value: float, min: float = -10.0, max: float = 10.0) -> None:
-        self.value = value
-        self.min = min
-        self.max = max
+    def __init__(self, value: float, bounds: tuple[float, float]) -> None:
 
+        self.value = value
+        self.bounds = bounds
+        
     def update(self, value: float) -> None:
+        
         self.value = value
 
     def randomize(self) -> None:
-        self.value = uniform(self.min, self.max)
+        
+        self.value = uniform(*self.bounds)
 
-    def output(self) -> float:
+    def evaluate(self) -> float:
+        
         return self.value
 
 class SLayer:
 
-    def __init__(self, values: list[float], bounds: list[tuple[float, float]]) -> None:
-        self.s_points = [SPoint(values[i], bounds[i][0], bounds[i][1]) for i in range(len(values))] 
+    def __init__(self, nodes: list[SPoint]) -> None:
+        self.nodes = nodes 
 
-    def size(self):
-        return len(self.x)
+    def size(self) -> int:
+        
+        return len(self.nodes)
     
-    def update(self, x: list[float]) -> None:
-        for i in range(len(self.size())):
-            self.x[i].update(x[i])
-
     def randomize(self) -> None:
-        for s_point in self.s_points:
-            s_point.randomize()
+        
+        for node in self.nodes:
+            node.randomize()
 
 class Perceptron:
             
-    def __init__(self, activation_threshold: float, output_value: float) -> None:
+    def __init__(self, threshold: float, output_value: float) -> None:
 
-        self.inputs = None        
-        self.input_weights = None
+        self.parent_nodes = None        
+        self.w = None
 
-        self.activation_threshold = activation_threshold
+        self.threshold = threshold
         self.output_value = output_value
 
-    def connect_inputs(self, inputs: list[SPoint | Perceptron], weights: list[float]):        
-        assert(len(inputs) == len(weights))
+    def connect_parents(self, nodes: list[SPoint | Perceptron], weights: list[float]):        
+        assert(len(nodes) == len(weights))
         
-        self.inputs = inputs
-        self.input_weights = weights
+        self.parent_nodes = nodes
+        self.w = weights
 
     def aggregate_input_value(self):
-        return sum([self.inputs[x].output() * self.input_weights[x] for x in range(len(self.inputs))])
+        return sum([self.parent_nodes[x].evaluate() * self.w[x] for x in range(len(self.parent_nodes))])
 
-    def is_active(self) -> bool:
-        return self.aggregate_input_value() >= self.activation_threshold
+    def z(self):
+        return self.aggregate_input_value() + self.threshold
 
-    def output(self) -> float:
-        return self.output_value if self.is_active() else 0
+    def activation_fn(z: float) -> float:
+        return 1.0 if z > 0.0 else 0.0
 
-def random_input_weights(n: int) -> list[float]:
-    return [0.0 for j in range(n)]
-
-def random_activation_threshold() -> float:
-    return 1.0
+    def evaluate(self) -> float:
+        return self.output_value * self.activation_fn(self.z())
 
 class ALayer:
     
-    def __init__(self, units) -> None:
-        self.units = units
+    def __init__(self, nodes: list[Perceptron]) -> None:
+        self.nodes = nodes
+        self.parent_layer: SLayer | ALayer | None = None
         
     def size(self):
-        return len(self.units)
+        return len(self.nodes)
 
+    def fully_connect_parent_layer(self, parent_layer: SLayer | ALayer, weights: list[float]):
+        
+        self.parent_layer = parent_layer
 
-    def connect_to(self):
+        for node in self.nodes:
+            node.connect_parents(parent_layer.nodes, weights)
 
-
-    # construct
-    # fully connect to
-    # - s_layer
-    # - parent a_layer
-
-
-class LinearClassifier:
-    pass
+class SimpleLinearClassifier:
 
     def __init__(self) -> None:
 
-        self.s_layer = SLayer([0.0, 0.0], [(-10, 10), (-10, 10)])
-        self.s_layer.randomize()
+        self.x_bounds = (0.0, 2.0)
+        x = SPoint(mean(self.x_bounds), self.x_bounds)
 
+        self.y_bounds = (0.0, 2.0)
+        y = SPoint(mean(self.y_bounds), self.y_bounds)
 
+        self.s_layer = SLayer([x, y])
 
+        # ax, by
+        s_node_weights = [-2.0, -2.0]
+
+        perceptron = Perceptron(threshold=3.0, output_value=1.0)
+        
+        self.a_layer = ALayer([perceptron])
+        self.a_layer.fully_connect_parent_layer(self.s_layer, s_node_weights)
+
+    def graph(self):
+
+        figure = pyplot.figure()
+        subplot = figure.add_subplot(111)
+
+        sample_size = 30
+
+        x_min = self.x_bounds[0]
+        x_max = self.x_bounds[1]
+        x_interval_size = x_max - x_min 
+        x_step_size = x_interval_size / float(sample_size)
+        
+        logging.info(f'x_min {x_min}, x_max {x_max}, x_interval_size {x_interval_size}, x_step_size {x_step_size}')
+    
+        x_ = [x_min + (i * x_step_size) for i in range(sample_size)]
+        
+        def graph_a_node(node: Perceptron, color = 'black'):
+        
+            a = node.w[0]
+            b = node.w[1]
+            c = node.threshold 
+
+            y_ = [-1.0 * (a * x + c) / b for x in x_]
+
+            line = lines.Line2D(x_, y_, color=color)
+            subplot.add_line(line)
+
+        for node in self.a_layer.nodes:
+            graph_a_node(node)
+
+        subplot.grid(True, which='both')
+
+        subplot.set_xlim(self.x_bounds)
+        subplot.set_ylim(self.y_bounds)
+
+        pyplot.show()
 
 def entrypoint():
 
-    slayer = SLayer([])    
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            # logging.FileHandler("debug.log"),
+            logging.StreamHandler()
+        ]
+    )
 
+    classifier = SimpleLinearClassifier()
+    classifier.graph()
 
-    n_layer_1 = 4
-    layer_1 = [
-        Perceptron(i, inputs=s_layer, input_weights=random_input_weights(n_s_layer), activation_threshold=random_activation_threshold(), output_value=1.0) 
-        for i in range(n_layer_1)
-    ]
-
-    layer_2 = [Perceptron(1, inputs=layer_1, input_weights=[1.0 for i in range(n_layer_1)], activation_threshold=1.0*n_layer_1, output=1.0)]
-
-
-    # update .value for each SPoint in the s_layer
-
-
-
-
-
+entrypoint()
